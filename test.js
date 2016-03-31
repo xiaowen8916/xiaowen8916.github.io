0438f7e00a3ce3ad0ef8ca9cdb23c54f66f2ccda
qiwenshi <qiwenshi@sohu-inc.com>
2016-03-31 15:51:54 +0800

    Site updated: 2016-03-31 15:51:53

--- test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by qiwenshi on 2016/3/15.
- */
-var fs=require('hexo-fs');
-var path=require('path');
-fs.readFile('source/_posts/hello-world.md',function(err,data){
-    console.log(data);
-});
